0b096a513cc39f2a0f94db2008d76814474f34ad
Mohamed Osman <2mspacee@gmail.com>
2025-05-08 00:06:26 +0300

    up

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@
 // element toggle function
 const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
 
-
+ 
 
 // sidebar variables
 const sidebar = document.querySelector("[data-sidebar]");
